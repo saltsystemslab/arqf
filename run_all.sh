91622d839752f114faf506d4eb0d35d9e4c52343
Yuvaraj Chesetti <yuvaraj.chesetti@gmail.com>
2024-12-05 23:23:38 -0500

    Trying out some query optimizations

--- run_all.sh
+++ run_all.sh
@@ -7,5 +7,5 @@ dataset_size=$1 #small/medium/large
 ./bench/scripts/gen_fpr_workload.sh medium ./workloads
 ./bench/scripts/run_fpr.sh medium workloads results
 
-#./bench/scripts/gen_fpr_workload.sh large workloads
-#./bench/scripts/perf_fpr.sh large workloads
+./bench/scripts/gen_fpr_workload.sh large workloads
+./bench/scripts/perf_fpr.sh large workloads
