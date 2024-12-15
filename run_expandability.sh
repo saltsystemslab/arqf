6b592ebad25a17d6307c7380ad60cf314942cd85
Yuvaraj Chesetti <yuvaraj.chesetti@gmail.com>
2024-12-15 16:43:07 +0000

    Update expandabability script

--- run_expandability.sh
+++ run_expandability.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#time ./bench/scripts/gen_expandability_workload.sh small workloads
-#time ./bench/scripts/run_expandability.sh small workloads disk
+time ./bench/scripts/gen_expandability_workload.sh small workloads
+time ./bench/scripts/run_expandability.sh small workloads disk
 time ./bench/scripts/gen_expandability_workload.sh medium workloads
 time ./bench/scripts/run_expandability.sh medium workloads disk
-#time ./bench/scripts/gen_expandability_workload.sh large workloads
-#time ./bench/scripts/run_expandability.sh large workloads disk
+time ./bench/scripts/gen_expandability_workload.sh large workloads
+time ./bench/scripts/run_expandability.sh large workloads disk
