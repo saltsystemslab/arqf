d9a06f501ef7c75262e2d6b75fc892ababc32216
Yuvaraj Chesetti <yuvaraj.chesetti@gmail.com>
2025-03-02 23:32:39 +0000

    Updating test scripts

--- bench/scripts/run_inserts.sh
+++ bench/scripts/run_inserts.sh
@@ -3,8 +3,8 @@ set -x
 
 BIN_DIR=./release
 BPK=16
-TEST_TYPE=fpr
-DATASET=$4
+TEST_TYPE=inserts
+DATASET=kuniform
 
 if [[ $1 == "small" ]]; then
 WORKLOAD_DIR=$2/small/${DATASET}
@@ -27,50 +27,50 @@ for dir in ${WORKLOAD_DIR}/*/
 do
   rm ${dir}memento_${TEST_TYPE}.csv
   rm ${dir}arqf_${TEST_TYPE}.csv
-  rm ${dir}adaptive_arqf_splinterdb_${TEST_TYPE}.csv
 done
 
 for dir in ${WORKLOAD_DIR}/*/
 do
 for bpk in 8 12 16 20 24 28
 do
+for qsize in 100000 1000000 10000000 100000000
+do
   mkdir -p ${RESULT_DIR}/${dir}
-
   ${BIN_DIR}/bench/bench_arqf $bpk \
     --keys ${WORKLOAD_DIR}/keys \
     --workload ${dir}left ${dir}right \
     --csv ${RESULT_DIR}/${dir}arqf_${TEST_TYPE}.csv \
-    --test-type ${TEST_TYPE} --key_len 8 --val_len 504 --buffer_pool_size ${CACHE_SIZE}
-
-  ${BIN_DIR}/bench/bench_adaptive_arqf_inmem $bpk \
-    --keys ${WORKLOAD_DIR}/keys \
-    --workload ${dir}left ${dir}right \
-    --csv ${RESULT_DIR}/${dir}adaptive_arqf_inmem_${TEST_TYPE}.csv \
+    --max-queries $qsize \
     --test-type ${TEST_TYPE} --key_len 8 --val_len 504 --buffer_pool_size ${CACHE_SIZE}
 
   ${BIN_DIR}/bench/bench_memento $bpk \
     --keys ${WORKLOAD_DIR}/keys \
     --workload ${dir}left ${dir}right \
     --csv ${RESULT_DIR}/${dir}memento_${TEST_TYPE}.csv \
+    --max-queries $qsize \
     --test-type ${TEST_TYPE} --key_len 8 --val_len 504 --buffer_pool_size ${CACHE_SIZE}
 
   ${BIN_DIR}/bench/bench_surf $bpk \
     --keys ${WORKLOAD_DIR}/keys \
     --workload ${dir}left ${dir}right \
     --csv ${RESULT_DIR}/${dir}surf_${TEST_TYPE}.csv \
+    --max-queries $qsize \
     --test-type ${TEST_TYPE} --key_len 8 --val_len 504 --buffer_pool_size ${CACHE_SIZE}
 
   ${BIN_DIR}/bench/bench_snarf $bpk \
     --keys ${WORKLOAD_DIR}/keys \
     --workload ${dir}left ${dir}right \
     --csv ${RESULT_DIR}/${dir}snarf_${TEST_TYPE}.csv \
+    --max-queries $qsize \
     --test-type ${TEST_TYPE} --key_len 8 --val_len 504 --buffer_pool_size ${CACHE_SIZE}
 
   ${BIN_DIR}/bench/bench_grafite $bpk \
     --keys ${WORKLOAD_DIR}/keys \
     --workload ${dir}left ${dir}right \
     --csv ${RESULT_DIR}/${dir}grafite_${TEST_TYPE}.csv \
+    --max-queries $qsize \
     --test-type ${TEST_TYPE} --key_len 8 --val_len 504 --buffer_pool_size ${CACHE_SIZE}
 
+done
 done
 done
